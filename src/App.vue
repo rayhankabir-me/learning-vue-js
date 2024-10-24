62c555ae1b71fe9dfe1572667ab67c37616065b9
rayhankabir-me <devrayhankabir@gmail.com>
2024-10-24 15:28:17 +0600

    vue js - options api

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello world',
+      status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'inactive'
+      }
+    },
+  },
+}
+</script>
+<template>
+  <h1>{{ message }}</h1>
+  <p v-if="status === 'active'">user is active</p>
+  <p v-else-if="status === 'pending'">user is pending</p>
+  <p v-else>user is inactive</p>
+  <h3>Tasks Lists:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <button v-on:click="toggleStatus">Toggle Status</button>
+</template>
